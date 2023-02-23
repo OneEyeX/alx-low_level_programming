db72aa64b1f0a9cb48f42e781e8960da14adba1a
OneEyeX <chedly199@gmail.com>
2023-02-23 05:07:33 -0800

    task4_4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,44 +1,20 @@
 #include "main.h"
 
 /**
- * _isdigit - a function that checks for a digit 0-9
- * @c: input
- * Return: 1 if c is a digit, 0 otherwise
+ * print_most_numbers - a function that prints the numbers, from 0 to 9
+ *
+ * Return 0-9, excluding 2 and 4, followed by a new line
  */
-int _isdigit(int c)
-{if ((c >= 48) && (c <= 57))
-                return (1);
-        else
-                return (0);
-}
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
-~
--- INSERT --                                          9,2           All
+void print_most_numbers(void)
+{
+	int i;
 
+	for (i = 0; i <= 9; i++)
+	{
+		if ((i == 2) || (i == 4))
+			continue;
+		else
+			_putchar(i + '0');
+	}
+	_putchar('\n');
+}
