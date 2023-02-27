114c63fc3c58dc7b1d1dc40e38c39c4d51935f4e
OneEyeX <chedly199@gmail.com>
2023-02-27 12:35:19 -0800

    task4_0

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,7 +2,7 @@
 
 /**
  * reset_to_98 - updates the value of n to 98
- * *n: integer pointer to a variable to be updated
+ * n: integer pointer to a variable to be updated
  *
  * Return: nothing
  */
